f4c77ee179100498d9351f249782fd225628c18b
Manoj Rawat <rishabhblm@gmail.com>
2024-01-21 17:48:15 +0530

    share added

--- src/components/CardFloaters.jsx
+++ src/components/CardFloaters.jsx
@@ -1,12 +1,22 @@
+
+function copyToClipBoard(){
+    navigator.clipboard.writeText("https://27rishabhagarwal.github.io/BusinessCard/")
+    alert("Copied to clipboard!")
+   }
+
 const CardFloaters = () => {
-    return (
-        <>
-        <a href="./assets/RishabhAgarwal.vcf" download="RishabhAgarwal" className="card-floaters floater-right-btn">
-            <p>Add to Contact</p>
-            <i class="fa-solid fa-plus"></i>
-        </a>
-        </>
-    );
+ return (
+     <>
+     <a href="./assets/RishabhAgarwal.vcf" download="RishabhAgarwal" className="card-floaters floater-right-btn">
+         <p>Add to Contact</p>
+         <i class="fa-solid fa-plus"></i>
+     </a>
+     <button id=".share-button" className="card-floaters floater-left-btn" onClick={copyToClipBoard}>
+         <i class="fa-solid fa-share"></i>
+     </button>
+        
+     </>
+ );
 }
 
 export default CardFloaters;
