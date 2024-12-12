216f3531bc9a45fc781e382d6bc577e1b26d4c82
themanojrawat <mmkumarrawat8055@gmail.com>
2024-12-12 16:58:44 +0530

    vite update

--- src/components/CardButtons.jsx
+++ src/components/CardButtons.jsx
@@ -1,47 +1,49 @@
-import React from 'react';
+import React from "react";
 
-    const CardButtonBox = () => {
-        const handleButtonClick = (link) => {
-            if (navigator.userAgent.match(/Android/i)) {
-                window.open(link.android, '_blank');
-            } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-                window.open(link.ios, '_blank');
-            }
-        };
+const CardButtonBox = () => {
+  const handleButtonClick = (link) => {
+    if (navigator.userAgent.match(/Android/i)) {
+      window.open(link.android, "_blank");
+    } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+      window.open(link.ios, "_blank");
+    }
+  };
 
-        const apps = [
-            {
-                name: 'HP Pay',
-                android: 'https://play.google.com/store/apps/details?id=com.drivetrackplusrefuel',
-                ios: 'https://apps.apple.com/in/app/hp-pay/id1343241227',
-            },
-            {
-                name: 'Drive Track Plus',
-                android: 'https://play.google.com/store/apps/details?id=com.drivetrackplus',
-                ios: 'https://apps.apple.com/in/app/drivetrack-plus-customer-app/id1061140479',
-            },
-        ];
+  const apps = [
+    {
+      name: "HP Pay",
+      android:
+        "https://play.google.com/store/apps/details?id=com.drivetrackplusrefuel",
+      ios: "https://apps.apple.com/in/app/hp-pay/id1343241227",
+    },
+    {
+      name: "Drive Track Plus",
+      android:
+        "https://play.google.com/store/apps/details?id=com.drivetrackplus",
+      ios: "https://apps.apple.com/in/app/drivetrack-plus-customer-app/id1061140479",
+    },
+  ];
 
-        return (
-            <div className="card-button-box">
-                <div className="card-button-box-title">
-                    <h2>Partner Apps</h2>
-                    <p>Install to get extra benefits</p>
-                </div>
-                <div className="card-button-box-items">
-                    {apps.map((app, index) => (
-                        <a
-                            key={index}
-                            href="# "
-                            className="button"
-                            onClick={() => handleButtonClick(app)}
-                        >
-                            {app.name}
-                        </a>
-                    ))}
-                </div>
-            </div>
-        );
-    };
+  return (
+    <div className="card-button-box">
+      <div className="card-button-box-title">
+        <h2>Partner Apps</h2>
+        <p>Install to get extra benefits</p>
+      </div>
+      <div className="card-button-box-items">
+        {apps.map((app, index) => (
+          <a
+            key={index}
+            href="#"
+            className="button"
+            onClick={() => handleButtonClick(app)}
+          >
+            {app.name}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    export default CardButtonBox;
+export default CardButtonBox;
--- src/components/CardSocials.jsx
+++ src/components/CardSocials.jsx
@@ -1,24 +1,50 @@
 const CardSocials = () => {
-    return (
-        <div className="card-socials">
-            <div className="card-socials-title">
-                <h2>Social Links</h2>
-                <p>Follow me on social media</p>
-            </div>
-            <div className="card-socials-items">
-                <a href="https://m.facebook.com/profile.php/?id=100021529052398" target="_blank" rel="noreferrer">
-                    <img src="./assets/icons/fb_icon@72x.png" alt="instagram logo" />
-                    <span>Facebook</span>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </a>
-                <a href="http://www.instagram.com/rishabh275_" target="_blank" rel="noreferrer">
-                    <img src="./assets/icons/insta_icon@72x.png" alt="instagram logo" />
-                    <span>Instagram</span>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="card-socials">
+      <div className="card-socials-title">
+        <h2>Social Links</h2>
+        <p>Follow me on social media</p>
+      </div>
+      <div className="card-socials-items">
+        <a
+          href="https://m.facebook.com/profile.php/?id=100021529052398"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="./assets/icons/fb_icon@72x.png" alt="instagram logo" />
+          <span>Facebook</span>
+          <i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              width="20"
+              height="20"
+            >
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
+          </i>
+        </a>
+        <a
+          href="http://www.instagram.com/rishabh275_"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="./assets/icons/insta_icon@72x.png" alt="instagram logo" />
+          <span>Instagram</span>
+          <i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              width="20"
+              height="20"
+            >
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
+          </i>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default CardSocials;
